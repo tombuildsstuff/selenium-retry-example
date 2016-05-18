4caf2a5f11264f5bbb676a15d9530e6a32e11e59
tombuildsstuff <git@ibuildstuff.co.uk>
2016-05-18 10:11:05 +0300

    Initial commit

---
+++ SeleniumRetryExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumRetryExample
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Foo();
+    }
+
+    public static void Foo()
+    {
+      IWebElement element = null; // TODO: found through some other means
+
+      element.AttemptToPerformAction(() =>
+      {
+        element.Click();
+      }, 3, 5);
+    }
+  }
+}
+
+
---
+++ SeleniumRetryExample/RetryExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace SeleniumRetryExample
+{
+  public static class RetryExample
+  {
+    public static void AttemptToPerformAction(this IWebElement element, Action action, int numberOfTimesToRetry = 3, int secondsBetweenRetries = 5)
+    {
+      for (var attempt = 0; attempt <= numberOfTimesToRetry ; attempt++)
+      {
+        try
+        {
+          action();
+        }
+        catch
+        {
+          if (attempt < numberOfTimesToRetry) {
+            continue;
+          }
+
+          throw;
+        }
+      }
+    }
+  }
+}
